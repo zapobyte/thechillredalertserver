cca5438aa66f67a5d2c08ac861a1ea5bde702eb1
victor <contact@victorz.me>
2020-05-08 14:43:48 +0100

    updated server to the latest version

--- Readme.md
+++ Readme.md
@@ -18,19 +18,16 @@ To join the game use the OpenRA luncher and select The Chill RA server from the 
 
 ### Installation ![](https://www.openra.net/images/icons/ra_64x64.png)  
 
-**Running on Raspbian**
 
-[Link](https://github.com/OpenRA/OpenRA/wiki/OpenRA-on-RaspberryPi) - If the game is not installed do the following:
+Install dependencies if missing
+
 ```
 sudo apt-get install mono-runtime libmono2.0-cil libsdl2-2.0 \ 
 	libmono-system-core4.0-cil libmono-system-drawing4.0-cil libmono-system-data4.0-cil \ 
 	libmono-system-numerics4.0-cil libmono-system-runtime-serialization4.0-cil \
 	libmono-system-xml-linq4.0-cil libgl1-mesa-dri liblua5.1-0
 ```
-`wget https://github.com/OpenRA/OpenRA/releases/download/release-20180307/openra_release.20180307_all.deb`
 
-`sudo dpkg -i openra_release.20180307_all.deb`
+After the game has been installed just run `launch-dedicated.sh` script. More details inside the script if you want to have it configured.
 
-After the game has been installed just run `my-dedicated.sh` script. More details inside the script if you want to have it configured.
-
-`pm2 start ./my-dedicated.sh`
+`pm2 start ./launch-dedicated.sh`
---
+++ launch-dedicated.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# example launch script, see https://github.com/OpenRA/OpenRA/wiki/Dedicated for details
+
+# Usage:
+#  $ ./launch-dedicated.sh # Launch a dedicated server with default settings
+#  $ Mod="d2k" ./launch-dedicated.sh # Launch a dedicated server with default settings but override the Mod
+#  Read the file to see which settings you can override
+
+Name="${Name:-"The Chill Islan RA server"}"
+Mod="${Mod:-"ra"}"
+ListenPort="${ListenPort:-"1234"}"
+AdvertiseOnline="${AdvertiseOnline:-"True"}"
+Password="${Password:-""}"
+
+RequireAuthentication="${RequireAuthentication:-"False"}"
+ProfileIDBlacklist="${ProfileIDBlacklist:-""}"
+ProfileIDWhitelist="${ProfileIDWhitelist:-""}"
+
+EnableSingleplayer="${EnableSingleplayer:-"False"}"
+EnableSyncReports="${EnableSyncReports:-"False"}"
+EnableGeoIP="${EnableGeoIP:-"True"}"
+ShareAnonymizedIPs="${ShareAnonymizedIPs:-"True"}"
+
+SupportDir="${SupportDir:-""}"
+
+while true; do
+     mono --debug OpenRA.Server.exe Game.Mod="$Mod" \
+     Server.Name="$Name" \
+     Server.ListenPort="$ListenPort" \
+     Server.AdvertiseOnline="$AdvertiseOnline" \
+     Server.EnableSingleplayer="$EnableSingleplayer" \
+     Server.Password="$Password" \
+     Server.GeoIPDatabase="$GeoIPDatabase" \
+     Server.RequireAuthentication="$RequireAuthentication" \
+     Server.ProfileIDBlacklist="$ProfileIDBlacklist" \
+     Server.ProfileIDWhitelist="$ProfileIDWhitelist" \
+     Server.EnableSyncReports="$EnableSyncReports" \
+     Server.EnableGeoIP="$EnableGeoIP" \
+     Server.ShareAnonymizedIPs="$ShareAnonymizedIPs" \
+     Engine.SupportDir="$SupportDir"
+done
